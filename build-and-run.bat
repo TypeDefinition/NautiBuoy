c68da21b5837c3b748246514e9d74295759984ea
TypeDefinition <lnxterry@gmail.com>
2021-05-11 22:24:39 +0800

    Added dev environment for Windows

--- build-and-run.bat
+++ build-and-run.bat
@@ -1,3 +1,5 @@
+if not exist ".\bin" mkdir .\bin
+if not exist ".\bin-int" mkdir .\bin-int
 START rgbasm -o ./bin-int/main.o ./src/test.asm
 START rgblink -o ./bin/prison-break.gb ./bin-int/main.o
 START rgbfix -v -p 0 ./bin/prison-break.gb
