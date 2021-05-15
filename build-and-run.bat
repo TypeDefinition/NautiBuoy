c260c2da0924e1ef65114461ffedd6df4bad54d8
Angie <angielimenqi7@gmail.com>
2021-05-15 21:56:39 +0800

    - modified .bat file to be able to run

--- build-and-run.bat
+++ build-and-run.bat
@@ -1,6 +1,6 @@
-if not exist "./bin" mkdir ./bin
-if not exist "./bin-int" mkdir ./bin-int
-START rgbasm -o ./bin-int/lapis-obj.o ./src/lapis.asm
-START rgblink -o ./bin/lapis.gb ./bin-int/lapis-obj.o
-START rgbfix -f lhg -p 255 ./bin/lapis.gb
-START ./bgb/bgb64.exe ./bin/lapis.gb
+if not exist ".\bin" mkdir .\bin
+if not exist ".\bin-int" mkdir .\bin-int
+START rgbasm -o ./bin-int/main.o ./src/lapis.asm
+START rgblink -o ./bin/game.gb ./bin-int/main.o
+START rgbfix -v -p 0 ./bin/game.gb
+START ./bgb/bgb64.exe ./bin/game.gb
