264c7587ca2d61eec167b5e8a9b7ffd77f59ec82
Angie <angielimenqi7@gmail.com>
2021-05-17 17:00:21 +0800

    - extracted and made a memcopy function along with a copy to tileset function - Moved the helloWorldStr to assets.asm instead

--- src/assets.asm
+++ src/assets.asm
@@ -3,3 +3,15 @@ SECTION "Assets", ROM0
 FontTiles::
     INCBIN "./src/tileset.ts" ; INCBIN copies the file contents directly into the ROM 
 FontTilesEnd::
+
+TESTSPRITE::
+    INCBIN "./src/2bppFormat/TestSprite.png.2bpp"
+.end
+
+HelloWorldStr::
+    db "Angie is awesome!"
+    ds 15
+    db "Next line."
+    ds 22
+    db $01, $01, $01, $01, $FF
+    
