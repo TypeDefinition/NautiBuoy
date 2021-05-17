bcc074f48c81a9af26b3420aa204f56ad1ac14b6
Angie <angielimenqi7@gmail.com>
2021-05-17 16:39:19 +0800

    - just edited the script to help generate .2bpp format files

--- assets/generate-2bppFormat.bat
+++ assets/generate-2bppFormat.bat
@@ -1,5 +1,5 @@
-rm -r 2bppFormat
-mkdir 2bppFormat
+rm -r ..\src\2bppFormat
+mkdir ..\src\2bppFormat
 
 cd Sprites
-for %%i in (*.png); do rgbgfx -o ..\2bppFormat\%%i.2bpp %%i
+for %%i in (*.png); do rgbgfx -o ..\..\src\2bppFormat\%%i.2bpp %%i
