0dcd6a8c29b0bcf429ecbe7cca9bb8726ada7d86
TypeDefinition <lnxterry@gmail.com>
2021-05-18 18:35:01 +0800

    Fixed build-and-run.bat

--- build-and-run.bat
+++ build-and-run.bat
@@ -2,5 +2,11 @@ if not exist ".\bin" mkdir .\bin
 if not exist ".\bin-int" mkdir .\bin-int
 if not exist ".\tile_data" mkdir .\tile_data
 
-for %%f in (.\assets\*.png); do START rgbgfx -o ./tile_data/~%%n.2bpp %%f
-cmd /k
+for /r ".\assets" %%i in (*.png); do rgbgfx -o .\tile_data\%%~ni.2bpp %%i
+for /r ".\src" %%i in (*.asm); do rgbasm -o .\bin-int\%%~ni.o %%i
+
+for /r ".\bin-int" %%i in (*.o); do call set "OBJFiles=%%OBJFiles%% %%~i"
+rgblink -o .\bin\lapis.gb -n .\bin-int\symbols.sym %OBJFiles%
+
+rgbfix -f lhg -p 255 .\bin\lapis.gb
+START .\bgb\bgb64.exe .\bin\lapis.gb
