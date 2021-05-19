b8397f22fea5d549df4406fcd3770778592696a6
TypeDefinition <lnxterry@gmail.com>
2021-05-19 20:29:40 +0800

    Fixed build-and-run typo

--- build-and-run.bat
+++ build-and-run.bat
@@ -9,4 +9,4 @@ for /r ".\bin-int" %%i in (*.o); do call set "OBJFiles=%%OBJFiles%% %%~i"
 rgblink -o .\bin\lapis.gb -n .\bin-int\symbols.sym %OBJFiles%
 
 rgbfix -f lhg -p 255 .\bin\lapis.gb
-java -jar ./Emulicious/Emulicious.jar
+java -jar .\Emulicious\Emulicious.jar .\bin\lapis.gb
