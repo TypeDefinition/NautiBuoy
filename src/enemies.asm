d740baf71419250d472ca14ba2303657b44a5b39
Angie <angielimenqi7@gmail.com>
2021-05-31 21:24:23 +0800

    - made a function to initialise enemies for each levels

--- src/enemies.asm
+++ src/enemies.asm
@@ -2,10 +2,12 @@ INCLUDE "./src/include/hardware.inc"
 INCLUDE "./src/include/structs.inc"
 INCLUDE "./src/include/entities.inc"
 INCLUDE "./src/include/definitions.inc"
+INCLUDE "./src/include/util.inc"
+
 
 
 SECTION "Enemies Data", WRAM0
-EnemiesData::
+wEnemiesData::
     dstruct Character, wEnemy0
     dstruct Character, wEnemy1
     dstruct Character, wEnemy2
@@ -14,6 +16,7 @@ EnemiesData::
     dstruct Character, wEnemy5
     dstruct Character, wEnemy6
     dstruct Character, wEnemy7
+wEnemiesDataEnd::
 
 
 ; just make it shoot/attack first?
@@ -26,7 +29,70 @@ EnemiesData::
 ; if enemy has been hit, make it dead
 ; should no longer be rendered
 
+; i need a way to set the enemy pos based on the map too
+
 SECTION "Enemies", ROM0
+
+/*  Read data on where enemy should be and its type
+    Initialise the enemy
+*/
+InitEnemiesAndPlaceOnMap::
+    mem_set_small wEnemiesData, 0, wEnemiesDataEnd - wEnemiesData ; reset all enemy data
+
+    ld hl, wEnemiesData
+    ld bc, LevelOneEnemyData
+    ld a, [bc] ; get number of enemies in level
+    ld d, a ; transfer the numbner of enemies to d
+    
+    inc bc
+.loop
+    ld a, [bc]
+    ld [hli], a ; store flag
+    inc bc
+
+    inc hl ; skip PosYInterpolateTarget, since init to 0 abv
+
+    ld a, [bc]
+    ld [hli], a ; set first byte of pos y
+    inc bc
+
+    inc hl ; skip second byte of pos y = 0
+    inc hl ; skip PosXInterpolateTarget = 0
+
+    ld a, [bc]
+    ld [hli], a ; set first byte of pos x
+    inc hl ;  skip second byte of pos x = 0
+    inc bc
+
+    ld a, [bc]
+    ld [hli], a ; set direction
+    inc bc
+
+    ld a, [bc]
+    ld [hli], a ; set health
+    inc bc
+
+    ld a, [bc]
+    ld [hli], a ; set first part of velocity
+    inc bc
+
+    ld a, [bc]
+    ld [hli], a ; set second part of velocity
+    inc bc
+
+    inc hl ; skip CurrAnimationFrame = 0
+
+    ld a, ENEMY_TYPEA_WALK_FRAMES ; TEMP CODES, initialised this properly
+    ld [hli], a ; set CurrStateMaxAnimFrame
+
+    dec d
+    ld d, a
+    cp a, 0 ; check if initialise all the required enemies yet
+    jr nz, .loop
+.endloop
+    ret
+
+
 UpdateAllEnemies::
     ld hl, EnemiesData
 
@@ -39,6 +105,7 @@ UpdateAllEnemies::
     jr nz, .updateEnemy ; if not alive, redo loop
 
 .updateEnemy
+    ;push hl
     and a, BIT_MASK_TYPE
     
 .enemyTypeA
@@ -47,7 +114,7 @@ UpdateAllEnemies::
     call UpdateEnemyA ; call correct update for enemy
 .enemyTypeB
     call UpdateEnemyB ; call correct update for enemy
-    
+
 .endOfLoop
     ret
 
@@ -57,6 +124,46 @@ UpdateAllEnemies::
     - hl: the starting address of the enemy
 */
 UpdateEnemyA:
+    ; just be able to shoot first maybe?
+
+    ; be able to render too
+
+/*
+.attack
+    call GetNonActiveBullet 
+
+    ld a, [hl]
+    bit BIT_FLAG_ACTIVE, a ; check if alive
+    jr z, .finishAttack ; if not available bullets just finish attack
+
+    ; set the variables
+    ld a, FLAG_ACTIVE | FLAG_ENEMY
+    ld [hli], a ; its alive
+
+    ld a, [wPlayer_Direction]
+    ld [hli], a ; direction
+
+    ; TODO:: SET VELOCITY FOR BULLET BASED ON TYPE LATER
+    ld a, $02
+    ld [hli], a ; velocity
+    xor a
+    ld [hli], a ; second part of velocity
+
+    ld a, [wPlayer_PosY] 
+    ld [hli], a ; pos Y
+    
+    ld a, [wPlayer_PosY + 1] 
+    ld [hli], a ; load the other half of posY
+
+    ld a, [wPlayer_PosX]
+    ld [hli], a ; pos x
+
+    ld a, [wPlayer_PosX + 1] 
+    ld [hli], a ; load the other half of posX
+
+.finishAttack */
+
+
     ret
 
 /* Update for enemy type B */
---
+++ src/enemies_level_data.asm
@@ -0,0 +1,24 @@
+
+INCLUDE "./src/include/definitions.inc"
+
+
+/*  Stores the data on where and what type of enemy should be place on the level 
+    Data for enemies:
+        - Enemy type
+        - Grid pos y * 8 pixels
+        - Grid pos x * 8 pixels
+        - Direction maybe?
+        - health
+        - velocity
+*/
+SECTION "Enemies Level Data", ROMX, BANK[2]
+LevelOneEnemyData::
+    db 2 ; number of enemies in level
+.enemyOne
+    db TYPE_ENEMYA | FLAG_ENEMY | FLAG_ACTIVE   
+    db 10 * 8
+    db 6 * 8
+    db DIR_UP
+    db ENEMY_TYPEA_HEALTH
+    dw VELOCITY_SLOW
+.endLevelOneEnemyData::
