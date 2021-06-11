55d2dc919f45dae07d2289c6a0e05ddd2010aa08
TypeDefinition <lnxterry@gmail.com>
2021-06-12 00:05:52 +0800

    Undoing a fuckup

--- build-and-run.sh
+++ build-and-run.sh
@@ -7,7 +7,7 @@ mkdir -p ./tile_data
 find ./assets/background -type f -name '*.png' -exec sh -c 'rgbgfx -o ./tile_data/$(basename "{}" .png).2bpp $0' {} \;
 find ./assets/sprites -type f -name '*.png' -exec sh -c 'rgbgfx -h -o ./tile_data/$(basename "{}" .png).2bpp $0' {} \;
 find ./src/ -type f -name '*.asm' -exec sh -c 'rgbasm -o ./bin-int/$(basename "{}" .asm).o $0' {} \;
-find ./bin-int/ -type f -name '*.o' -exec rgblink -o ./bin/gbgame.gb -n ./bin-int/symbols.sym -m ./bin/memory_data.map {} +
+find ./bin-int/ -type f -name '*.o' -exec rgblink -o ./bin/gbgame.gb -n ./bin/gbgame.sym -m ./bin/gbgame.map {} +
 rgbfix -f lhg -p 255 ./bin/gbgame.gb
 # wine64 ./bgb/bgb64.exe ./bin/gbgame.gb
 java -jar ./Emulicious/Emulicious.jar ./bin/gbgame.gb
