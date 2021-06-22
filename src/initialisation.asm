5e134b23b95fcda34e0a81d76e795b115001bcf1
TypeDefinition <lnxterry@gmail.com>
2021-06-22 17:32:35 +0800

    FUCK THIS STUPID FUCKING PIECE OF SHIT.

--- src/initialisation.asm
+++ src/initialisation.asm
@@ -24,7 +24,7 @@ LCDOn::
     push af
     ; Turn screen on, display background
     ; WARNING/TEMP: might change one of the 9800 to use tilemap 9c00, unsure what effects putting both to 9800 will cause for UI. default is 9800 
-    ld a, LCDCF_ON | LCDCF_WIN9C00 | LCDCF_WINOFF | LCDCF_BG9800 | LCDCF_OBJ16 | LCDCF_OBJON | LCDCF_BGON ; we want to set back LCDC bit 7 to 1
+    ld a, LCDCF_ON | LCDCF_WIN9C00 | LCDCF_WINON | LCDCF_BG9800 | LCDCF_OBJ16 | LCDCF_OBJON | LCDCF_BGON ; we want to set back LCDC bit 7 to 1
     ld [rLCDC], a ; turn on the screen
     pop af
     ret
@@ -80,7 +80,7 @@ Initialise::
     mem_copy Level0, GameLevelTiles, Level0.end-Level0
     mem_copy GameLevelTiles, _SCRN0, GameLevelTiles.end-GameLevelTiles
 
-    ; call InitialiseGameUI
+    call InitialiseGameUI
 
     ; TEMP: Temporary code.
     set_romx_bank 2
