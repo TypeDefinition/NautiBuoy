d96ec5d12f4255858ff28dc1473b0a07f6f003cb
TypeDefinition <lnxterry@gmail.com>
2021-06-30 23:37:47 +0800

    Can't believe nothing broke yet.

--- src/game_level.asm
+++ src/game_level.asm
@@ -79,4 +79,33 @@ LoadGameLevel::
     ldh [rIF], a
     ei ; Enable Master Interrupt Switch
 
-    jp UpdateLoop
+    jp UpdateGameLevel
+
+UpdateGameLevel::
+    set_romx_bank BANK(Sprites) ; player, enemy and bullet sprite data is in rombank 2
+    ;ld a, BANK(LevelOneEnemyData)
+    ;ld [rROMB0], a
+    call UpdateInput
+
+    call ResetShadowOAM
+
+    ; insert game logic here and update shadow OAM data
+    call UpdatePlayerMovement
+    call UpdatePlayerAttack
+    call UpdatePlayerCamera
+    call UpdatePlayerShadowOAM
+
+    call UpdateAllEnemies    
+    call UpdateBullets
+    call UpdatePowerUpShadowOAM
+
+    ; Dirty tiles get updated during HBlank.
+    call UpdateDirtyTiles
+
+    ; Update Sound
+    set_romx_bank BANK(CombatBGM)
+    call _hUGE_dosound
+
+    rst $0010 ; Wait VBlank
+
+    jr UpdateGameLevel
