0be3453d193a627b1c160aaa2c7afd056952affd
TypeDefinition <lnxterry@gmail.com>
2021-07-09 03:52:22 +0800

    Save system WIP

--- src/save_game.asm
+++ src/save_game.asm
@@ -1,4 +1,5 @@
 INCLUDE "./src/include/hardware.inc"
+INCLUDE "./src/include/util.inc"
 
 DEF MAX_STAGES EQU 16
 
@@ -7,71 +8,152 @@ DEF MAX_STAGES EQU 16
     game stage.
 
     The game stage save data consists of the follwing:
-    Stage Locked/Unlock - 1 Byte (Locked = 0, Unlocked = 1)
+    Stage Locked/Unlock - 1 Byte (Locked = 0, Unlocked & Incomplete = 1, Unlocked & Complete = 2)
     Best Time Left - 2 Bytes
     Number of Stars - 1 Byte */
 SECTION "Save Game SRAM", SRAM
+sChecksum::
+    ds 1 ; Validation Checksum
 sSaveData::
-    ds 4 ; Validation string.
     ds 4*MAX_STAGES ; Save data for stages.
 .end::
 
 SECTION "Save Game WRAM", WRAM0
-wSaveStageNumber::
+wRWIndex::
     ds 1
-wSaveStageData::
+wRWData::
     ds 4
-
-SECTION "Save Game", ROM0
-SaveValidationString::
-    db "L", "E", "Q", "A"
 .end::
 
-EnableSRAM::
+SECTION "Save Game", ROM0
+EnableSRAM:
     ld a, CART_SRAM_ENABLE
     ld [rRAMG], a
     ret
 
-DisableSRAM::
+DisableSRAM:
     xor a
     ld [rRAMG], a
+    ret
+
+GenerateChecksum:
+    ld b, sSaveData.end - sSaveData
+    ld hl, sSaveData
+
+    ; Calculate checksum.
+    ld de, $0000 ; de = checksum
+.loop
+    ld a, [hli]
+    add a, e
+    ld e, a
+    ld a, d
+    adc a, $00
+    ld d, a
+
+    dec b
+    jr nz, .loop
+
+    ; Write checksum to SRAM.
+    ld d, a
+    ld [sChecksum], a
+    ld e, a
+    ld [sChecksum], a
+    ret
+
+; Validate the save data checksum.
+; @return Z Flag (Valid = Z, Invalid = NZ)
+ValidateChecksum:
+    ld b, sSaveData.end - sSaveData
+    ld hl, sSaveData
+
+    ; Calculate checksum. de = checksum
+    ld de, $0000
+.loop
+    ld a, [hli]
+    add a, e
+    ld e, a
+    ld a, d
+    adc a, $00
+    ld d, a
+
+    dec b
+    jr nz, .loop
+
+    ; Write checksum to SRAM.
+    ld d, a
+    ld [sChecksum], a
+    ld e, a
+    ld [sChecksum], a
+    ret
+
+    ; Get SRAM checksum. hl = SRAM checksum.
+    ld a, [sChecksum]
+    ld h, a
+    ld a, [sChecksum+1]
+    ld l, a
+
+    ; Check that hl == de.
+    ld a, l
+    cp e
+    ret nz
+    ld a, h
+    cp d
+    ret
+
+GenerateDefaultSaveGame:
+    ; Lock all stages.
+    mem_set_small sSaveData, $00, sSaveData.end - (sSaveData+1)
+    ; Unlock stage 1
+    ld a, $01
+    ld [sSaveData], a
     ret
 
 SaveGame::
     call EnableSRAM
 
-    ; Write to validation string.
-    ld a, [SaveValidationString]
-    ld [sSaveData], a
-    ld a, [SaveValidationString+1]
-    ld [sSaveData+1], a
-    ld a, [SaveValidationString+2]
-    ld [sSaveData+2], a
-    ld a, [SaveValidationString+3]
-    ld [sSaveData+3], a
-
     ; Set hl to the write memory address.
     ld bc, sSaveData
-    ld a, [wSaveStageNumber]
-    inc a
-    ld l, a
     ld h, $00
+    ld a, [wRWIndex]
+    ld l, a
     add hl, hl
     add hl, hl
     add hl, bc
     
     ; Write to SRAM
-    ld a, [wSaveStageData]
+    ld a, [wRWData]
     ld [hli], a
-    ld a, [wSaveStageData+1]
+    ld a, [wRWData+1]
     ld [hli], a
-    ld a, [wSaveStageData+2]
+    ld a, [wRWData+2]
     ld [hli], a
-    ld a, [wSaveStageData+3]
+    ld a, [wRWData+3]
     ld [hl], a
 
+    call GenerateChecksum
     call DisableSRAM
     ret
 
 LoadGame::
+    call EnableSRAM
+
+    call ValidateChecksum
+    call nz, GenerateDefaultSaveGame
+
+    ; Set hl to the write memory address.
+    ld bc, sSaveData
+    ld h, $00
+    ld a, [wRWIndex]
+    ld l, a
+    add hl, hl
+    add hl, hl
+    add hl, bc
+
+    ; Read from SRAM
+FOR N, wRWData.end - wRWData
+    ld a, [hli]
+    ld [wRWData+N], a
+ENDR
+
+    call DisableSRAM
     ret
