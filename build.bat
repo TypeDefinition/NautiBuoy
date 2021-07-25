00013336e89b8d6d6ae326b01d4009f18c680c25
TypeDefinition <lnxterry@gmail.com>
2021-07-25 22:42:39 +0800

    Added build script

---
+++ build.bat
@@ -0,0 +1,12 @@
+if not exist ".\bin" mkdir .\bin
+if not exist ".\bin-int" mkdir .\bin-int
+if not exist ".\tile_data" mkdir .\tile_data
+
+for /r ".\assets\background_and_ui" %%i in (*.png); do rgbgfx -o .\tile_data\%%~ni.2bpp %%i
+for /r ".\assets\sprites" %%i in (*.png); do rgbgfx -h -o .\tile_data\%%~ni.2bpp %%i
+for /r ".\src" %%i in (*.asm); do rgbasm -o .\bin-int\%%~ni.o %%i
+
+for /r ".\bin-int" %%i in (*.o); do call set "OBJFiles=%%OBJFiles%% %%~i"
+rgblink -o .\bin\gbgame.gb -n .\bin\gbgame.sym -m .\bin\gbgame.map %OBJFiles%
+
+rgbfix -f lhg -p 255 .\bin\gbgame.gb
---
+++ build.sh
@@ -0,0 +1,11 @@
+# Understanding the "-exec" option of "find": https://unix.stackexchange.com/questions/389705/understanding-the-exec-option-of-find/389706
+# Seperate filename & path inside "find"'s' "-exec" option: https://unix.stackexchange.com/questions/178217/separate-filename-and-path-inside-find-commands-exec-option
+mkdir -p ./bin
+mkdir -p ./bin-int
+mkdir -p ./tile_data
+
+find ./assets/background_and_ui -type f -name '*.png' -exec sh -c 'rgbgfx -o ./tile_data/$(basename "{}" .png).2bpp $0' {} \;
+find ./assets/sprites -type f -name '*.png' -exec sh -c 'rgbgfx -h -o ./tile_data/$(basename "{}" .png).2bpp $0' {} \;
+find ./src/ -type f -name '*.asm' -exec sh -c 'rgbasm -o ./bin-int/$(basename "{}" .asm).o $0' {} \;
+find ./bin-int/ -type f -name '*.o' -exec rgblink -o ./bin/gbgame.gb -n ./bin/gbgame.sym -m ./bin/gbgame.map {} +
+rgbfix -f lhg -p 255 ./bin/gbgame.gb
