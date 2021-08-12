68f416980d4ff84f84ebbec34d37e7cf89496e53
LapisRaider <angielimenqi7@gmail.com>
2021-08-13 00:22:05 +0800

    - added FX for hitting enemies and boss ram attack

--- src/audio/sfx.asm
+++ src/audio/sfx.asm
@@ -29,7 +29,7 @@ PlayerShootSFX::
     ; Channel 1
     ld a, %01001100
     ld [rNR10], a ; Sweep Register
-    
+
     ld a, %10100100
     ld [rNR11], a ; Set Sound Length/Wave Pattern Duty
 
@@ -105,6 +105,75 @@ EnemyDeathSFX::
     ld a, $44
     ld [rNR13], a ; Frequency Lo
 
+    ld a, %11000110
+    ld [rNR14], a ; Frequency Hi
+
+    pop af
+    ret
+
+/* Hit an enemy sound effect */
+EnemyHitSFX::
+    push af
+
+    ; Channel 1
+    ld a, %01000111
+    ld [rNR10], a ; Sweep Register
+    
+    ld a, %00001100 
+    ld [rNR11], a ; Set Sound Length/Wave Pattern Duty
+    
+    ld a, %11000010
+    ld [rNR12], a ; Volume Envelope
+    
+    ld a, $11
+    ld [rNR13], a ; Frequency Lo
+    
+    ld a, %11000110
+    ld [rNR14], a ; Frequency Hi
+
+    pop af
+    ret
+
+/* Hit an enemy sound effect */
+BossRamAttack::
+    push af
+
+    ; Channel 1
+    ld a, %11100110
+    ld [rNR10], a ; Sweep Register
+    
+    ld a, %10001111
+    ld [rNR11], a ; Set Sound Length/Wave Pattern Duty
+    
+    ld a, %11110010
+    ld [rNR12], a ; Volume Envelope
+    
+    ld a, $22
+    ld [rNR13], a ; Frequency Lo
+    
+    ld a, %11000110
+    ld [rNR14], a ; Frequency Hi
+
+    pop af
+    ret
+
+/* Hit an enemy sound effect */
+BossBarrageAttack::
+    push af
+
+    ; Channel 1
+    ld a, %11100110
+    ld [rNR10], a ; Sweep Register
+    
+    ld a, %10001111
+    ld [rNR11], a ; Set Sound Length/Wave Pattern Duty
+    
+    ld a, %11110010
+    ld [rNR12], a ; Volume Envelope
+    
+    ld a, $22
+    ld [rNR13], a ; Frequency Lo
+    
     ld a, %11000110
     ld [rNR14], a ; Frequency Hi
 
